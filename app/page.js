9f23b4388055a91edc5b42169271367c1e004a63
Velcola <89658692+Velcola@users.noreply.github.com>
2024-09-06 11:41:23 +0200

    routing

--- app/page.js
+++ app/page.js
@@ -1,23 +1,41 @@
 import Link from "next/link";
 
 export default function Home() {
+  const quizTypes = {
+    history: "Historie Quiz",
+    geography: "Geografi Quiz",
+    ita: "2ITA Quiz"
+  }
   return (
-    <main class="bg h-screen flex justify-center items-center">
-      <div class="w-full max-w-lg flex flex-col justify-center items-center text-center px-4">
-        <h1 class="text-3xl text-white roboto-medium mb-3">Velg en Quiz..</h1>
-        <div class="w-full primary text-white shadow-md rounded-lg">
-          <div class="m-3 flex flex-col gap-2">
-            <Link href='/quiz' legacyBehavior>
-              <button class="secondary rounded-lg shadow-sm p-2">
+    <main className="bg h-screen flex justify-center items-center">
+      <div className="w-full max-w-lg flex flex-col justify-center items-center text-center px-4">
+        <h1 className="text-3xl text-white roboto-medium mb-3">Velg en Quiz..</h1>
+        <div className="w-full primary text-white shadow-md rounded-lg">
+          <div className="m-3 flex flex-col gap-2">
+            <Link href={{
+              pathname: '/quiz',
+              query: { type: quizTypes.history }
+            }} legacyBehavior>
+              <button className="secondary rounded-lg shadow-sm p-2">
                   .•°¤* Historie Quiz *¤°•.
               </button>
             </Link>
-            <button class="secondary rounded-lg shadow-sm p-2">
-                .•°¤* 2ITA Quiz *¤°•.
-            </button>
-            <button class="secondary rounded-lg shadow-sm p-2">
-                .•°¤* Idfk *¤°•.
-            </button>
+            <Link href={{
+              pathname: '/quiz',
+              query: { type: quizTypes.ita }
+            }} legacyBehavior>
+              <button className="secondary rounded-lg shadow-sm p-2">
+                  .•°¤* 2ITA Quiz *¤°•.
+              </button>
+            </Link>
+            <Link  href={{
+              pathname: '/quiz',
+              query: { type: quizTypes.geography }
+            }} legacyBehavior>
+              <button className="secondary rounded-lg shadow-sm p-2">
+                  .•°¤* Geografi *¤°•.
+              </button>
+            </Link>
           </div>
         </div>
       </div>
--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,8 +1,15 @@
+'use client'
+import { useSearchParams } from "next/navigation";
 import React from "react";
+import fs from "fs";
 
-const page = () => {
+const  page = () => {
+    const file = fs.readFile(process.cwd() + 'app/data.json', 'utf-8')
+    const searchParams = useSearchParams();
+    const type = searchParams.get('type');
+
     return (
-        <div>Quiz page</div>
+        <h1>{type}</h1>
     );
 };
 
